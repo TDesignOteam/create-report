2715bf09435d0b789a1e2c91a6e271e4fb9395bf
94dreamer <503633021@qq.com>
2022-03-18 13:44:41 +0800

    fix: update issue

--- daily-issue.js
+++ daily-issue.js
@@ -1,4 +1,5 @@
 const core = require("@actions/core");
+const github = require('@actions/github');
 const { Octokit } = require("@octokit/rest");
 const { exec } = require("child_process");
 const { ReposEnum } = require("./const");
@@ -9,27 +10,18 @@ const data = core.getInput("data");
 
 const octokit = new Octokit({ auth: token });
 
-function renderMark(data) {
-  return `### ${data.title}
+function renderMark() {
+  return `> **有人提issue啦**
+> **标  题:** ${github.payload.issue.title}
+> **发起人:** $ORANGE_ISSUE_OWNER
+> [查看详情]($ORANGE_EVENT_URL)
 `
 }
 
-// async function getRepoIssue(repo) {
-//   return octokit.rest.issues
-//     .listForRepo({
-//       owner: "Tencent",
-//       repo: repo,
-//       state: "open",
-//       sort: "created",
-//       direction: "asc",
-//     })
-//     .then((res) => {
-//       res.data.repoName = repo;
-//       return res.data;
-//     });
-// }
-
 async function send() {
+  console.log(github.payload)
+  console.log(github.payload.issue)
+
   // 调取 参数指定的 ReposEnum 的issue 情况
   // 形成 infoData
   // 灌入模版 生成图表
